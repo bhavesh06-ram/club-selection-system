ecf82eb00e7e12362792d9e46169fd9181c69eb9
bhavesh06-ram <bshah911917@gmail.com>
2026-06-09 10:38:56 +0530

    server setup

--- server.js
+++ server.js
@@ -1,11 +1,99 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Club Selection System Running");
+app.use(express.urlencoded({ extended: true }));
+
+// MongoDB Connection
+
+mongoose.connect("mongodb+srv://admin:Club12345@cluster0.epje8tw.mongodb.net/clubDB?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Schema
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    branch: String,
+    year: String,
+    skills: String,
+    reason: String
 });
 
+// Model
+
+const Student = mongoose.model("Student", studentSchema);
+
+// Form Page
+
+app.get("/", (req, res) => {
+
+    res.send(`
+        
+        <h1>Club Coordinator Form</h1>
+
+        <form action="/submit" method="POST">
+
+            <input type="text" name="name" placeholder="Name" /><br><br>
+
+            <input type="email" name="email" placeholder="Email" /><br><br>
+
+            <input type="text" name="branch" placeholder="Branch" /><br><br>
+
+            <input type="text" name="year" placeholder="Year" /><br><br>
+
+            <input type="text" name="skills" placeholder="Skills" /><br><br>
+
+            <textarea name="reason" placeholder="Why join?"></textarea><br><br>
+
+            <button type="submit">Submit</button>
+
+        </form>
+
+    `);
+
+});
+
+// Save Form Data
+
+app.post("/submit", async (req, res) => {
+
+    await Student.create(req.body);
+
+    res.send("Form Submitted Successfully");
+
+});
+
+// Admin Page
+
+app.get("/admin", async (req, res) => {
+
+    const students = await Student.find();
+
+    let output = "<h1>All Applications</h1>";
+
+    students.forEach(student => {
+
+        output += `
+            <hr>
+            <h3>${student.name}</h3>
+            <p>${student.email}</p>
+            <p>${student.branch}</p>
+            <p>${student.skills}</p>
+            <p>${student.reason}</p>
+        `;
+    });
+
+    res.send(output);
+
+});
+
+// Start Server
+
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+
+    console.log("Server Running on Port 3000");
+
 });
